a5319a39a55b814e59a07b8781506af8ffa3b762
umikaze-games <c463384962@gmail.com>
2025-12-12 23:09:43 +0900

    commit

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerStateMachine : StateMachine
 {
 	[field:SerializeField]public InputReader InputReader { get; private set; }
+	[field: SerializeField] public CharacterController Controller { get; private set; }
+	[field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 	private void Start()
 	{
 		SwitchState(new PlayerTestState(this));
--- Assets/Scripts/StateMachines/StateMachine.cs
+++ Assets/Scripts/StateMachines/StateMachine.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public abstract class StateMachine : MonoBehaviour
 {
 	public State currentState;
+
 	private void Update()
 	{
 		if (currentState != null)
