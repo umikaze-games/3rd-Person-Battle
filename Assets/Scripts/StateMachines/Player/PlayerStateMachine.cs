9a8fbc3277102e29788a2b28e780e21833125959
umikaze-games <c463384962@gmail.com>
2025-12-16 20:58:16 +0900

    commit

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -5,8 +5,11 @@ public class PlayerStateMachine : StateMachine
 	[field:SerializeField]public InputReader InputReader { get; private set; }
 	[field: SerializeField] public CharacterController Controller { get; private set; }
 	[field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+	public Transform MainCameraTransform { get; private set; }
+
 	private void Start()
 	{
+		MainCameraTransform = Camera.main.transform;
 		SwitchState(new PlayerTestState(this));
 	}
 
