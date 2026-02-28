4945a7301c8ecff76d78bb6a88ddd48586d2e2e5
umikaze-games <c463384962@gmail.com>
2026-02-28 22:50:34 +0900

    commit

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+	protected EnemyStateMachine stateMachine;
+
+	public EnemyBaseState(EnemyStateMachine stateMachine)
+	{
+		this.stateMachine = stateMachine;
+	}
+
+}
